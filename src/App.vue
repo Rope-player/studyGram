3f187b9690b48d6a78e91d05ded7d195ec1d8367
Rope-player <38906192+Rope-player@users.noreply.github.com>
2019-09-21 12:58:41 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Calendar></Calendar>
+    <local-component></local-component>
+    <DMPicker></DMPicker>
+  </div>
+</template>
+
+<script>
+  import LocalComponent from './components/local-component'
+  import Calendar from './components/Calendar'
+  import DMPicker from './components/DMPicker'
+
+  export default {
+    name: 'app',
+    components: {Calendar}
+  }
+</script>
