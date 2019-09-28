a39d027ea7aa98745074807f3a87be7316e7d8ba
Rope-player <38906192+Rope-player@users.noreply.github.com>
2019-09-28 13:16:07 +0900

    Add files via upload

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -1,175 +1,45 @@
 <template>
-  <div class="calendar">
-      <h2>
-        <a href="#" v-on:click="onClickPrev(currentMonth)">◀</a>
-        {{currentYear}}년 {{currentMonth}}월
-        <a href="#" v-on:click="onClickNext(currentMonth)">▶</a>
-      </h2>
-      <table class="table table-hover">
-          <thead>
-            <tr>
-              <td v-for="(weekName, index) in weekNames" v-bind:key="index">
-                {{weekName}}
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(row, index) in currentCalendarMatrix" :key="index">
-              <td v-for="(day, index2) in row" :key="index2" style="padding:20px;">
-                <span v-if="isToday(currentYear, currentMonth, day)" class="rounded">
-                  {{day}}
-                </span>
-                <span v-else>
-                  {{day}}
-                </span>
-              </td>
-            </tr>
-          </tbody>
-      </table>    
-  </div>
+    <v-row>
+        <v-col cols="" sm="6" class="my-2 px-1">
+            <v-date-picker 
+                ref="picker"
+                v-model="date"
+                :picker-date.sync="pickerDate"
+                full-width
+                ></v-date-picker>
+        </v-col>
+        <v-col cols="12" sm="6" class="my-2 px-1">
+        <div class="title">이달의 행사 ({{ pickerDate || 'change month...' }})</div>
+        <div class="subheading">다른 일정을 보기위해서 월을 변경하세요.</div>
+        <ul class="ma-4">
+            <li v-for="note in notes" :key="note">{{ note }}</li>
+        </ul>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
-export default {
-  name: 'Calendar',
-  data () {
-    return {
-      weekNames: ["월요일", "화요일", "수요일","목요일", "금요일", "토요일", "일요일"],
-      rootYear: 1904,
-      rootDayOfWeekIndex: 4, // 2000년 1월 1일은 토요일
-      currentYear: new Date().getFullYear(),
-      currentMonth: new Date().getMonth()+1,
-      currentDay: new Date().getDate(),
-      currentMonthStartWeekIndex: null,
-      currentCalendarMatrix: [],
-      endOfDay: null,
-      memoDatas: [],
-    }
-  },
-  mounted(){
-      this.init();
-  },
-  methods: {
-      init:function(){
-        this.currentMonthStartWeekIndex = this.getStartWeek(this.currentYear, this.currentMonth);
-        this.endOfDay = this.getEndOfDay(this.currentYear, this.currentMonth);
-        this.initCalendar();
+  export default {
+    data: () => ({
+      date: new Date().toISOString().substr(0, 10),
+      pickerDate: null,
+      notes: [],
+      allNotes: [
+        '일정1',
+        '일정2',
+        '일정3',
+        '일정4',
+        '일정5',
+      ],
+    }),
+    watch: {
+      pickerDate (val) {
+        this.notes = [
+          this.allNotes[Math.floor(Math.random() * 5)],
+          this.allNotes[Math.floor(Math.random() * 5)],
+          this.allNotes[Math.floor(Math.random() * 5)],
+        ].filter((value, index, self) => self.indexOf(value) === index)
       },
-      initCalendar:function(){
-        this.currentCalendarMatrix = [];
-        let day=1;
-        for(let i=0; i<6; i++){
-          let calendarRow = [];
-          for(let j=0; j<7; j++){
-            if(i==0 && j<this.currentMonthStartWeekIndex){
-              calendarRow.push("");
-            }
-            else if(day<=this.endOfDay){
-              calendarRow.push(day);
-              day++;
-            }
-            else{
-              calendarRow.push("");
-            }
-          }
-          this.currentCalendarMatrix.push(calendarRow);
-        }
-      },
-      getEndOfDay: function(year, month){
-          switch(month){
-              case 1:
-              case 3:
-              case 5:
-              case 7:
-              case 8:
-              case 10:
-              case 12:
-                return 31;
-                break;
-              case 4:
-              case 6:
-              case 9:
-              case 11:
-                return 30;
-                break;
-              case 2:
-                if( (year%4 == 0) && (year%100 != 0) || (year%400 == 0) ){
-                return 29;   
-                }
-                else{
-                    return 28;
-                }
-                break;
-              default:
-                console.log("unknown month " + month);
-                return 0;
-                break;
-          }
-      },
-      getStartWeek: function(targetYear, targetMonth){
-        let year = this.rootYear;
-        let month = 1;
-        let sumOfDay = this.rootDayOfWeekIndex;
-        while(true){
-          if(targetYear > year){
-            for(let i=0; i<12; i++){
-              sumOfDay += this.getEndOfDay(year, month+i);
-            }
-            year++;
-          }
-          else if(targetYear == year){
-            if(targetMonth > month){
-              sumOfDay += this.getEndOfDay(year, month);
-              month++;
-            }
-            else if(targetMonth == month){
-              return (sumOfDay)%7;
-            }
-          }
-        }
-      },
-      onClickPrev: function(month){
-        month--;
-        if(month<=0){
-          this.currentMonth = 12;
-          this.currentYear -= 1;
-        }
-        else{
-          this.currentMonth -= 1;
-        }
-        this.init();
-      },
-      onClickNext: function(month){
-        month++;
-        if(month>12){
-          this.currentMonth = 1;
-          this.currentYear += 1;
-        }
-        else{
-          this.currentMonth += 1;
-        }
-        this.init();
-      },
-      isToday: function(year, month, day){
-        let date = new Date();
-        return year == date.getFullYear() && month == date.getMonth()+1 && day == date.getDate(); 
-      }
+    },
   }
-}
 </script>
-
-<style type="text/css">
-    .rounded {
-      -moz-border-radius:20px 20px 20px 20px; 
-      border-radius:20px 20px 20px 20px;
-      border:solid 1px #ffffff;
-      background-color:#cccccc;
-      padding:10px;
-      color:#ffffff;
-    }
-
-    .calendar {
-      height: 500px;
-      width: 880px;
-    }
-</style>
