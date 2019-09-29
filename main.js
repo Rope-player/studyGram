5f827fc0ca4a0bfb6e2031ebf813a4aac225165e
Rope-player <38906192+Rope-player@users.noreply.github.com>
2019-09-29 15:58:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
