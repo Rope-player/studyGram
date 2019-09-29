c7925a14dabc91df8f099571163a1c9406fb360d
Rope-player <38906192+Rope-player@users.noreply.github.com>
2019-09-29 15:57:29 +0900

    Update App.vue

--- App.vue
+++ App.vue
@@ -7,9 +7,7 @@
 </template>
 
 <script>
-  import LocalComponent from './components/local-component'
   import Calendar from './components/Calendar'
-  import DMPicker from './components/DMPicker'
 
   export default {
     name: 'app',
